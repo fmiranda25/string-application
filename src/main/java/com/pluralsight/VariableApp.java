4db6facdde809089635fa71ae24b37b948cf7c1b
Fernando <f.miranda25d@gmail.com>
2026-04-09 13:15:13 -0500

    Second commit with steps 1 and 2 complete

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+
+        String favColor = "Blue";
+        System.out.println(favColor);
+
+        int yearUpStart = 2026;
+        System.out.println(yearUpStart);
+
+        String midInit = " ";
+        System.out.println(midInit);
+
+        boolean hasPet = false;
+        System.out.println(hasPet);
+
+        String greeting =  "Top of the morning to ya lads!";
+        System.out.println(greeting);
+
+        byte daysInTheWeek = 7;
+        System.out.println(daysInTheWeek);
+
+        float coffeePrice = 4.99;
+        System.out.println(coffeePrice);
+
+        char favLetter = 'E';
+        System.out.println(favLetter);
+
+        boolean isRaining = false;
+        System.out.println(isRaining);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
