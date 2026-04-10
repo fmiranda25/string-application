def2af97bb8e8ff13f320ebbdc6c1f9ef6ce5d9b
Fernando <f.miranda25d@gmail.com>
2026-04-10 10:46:57 -0500

    Third commit with all 3 steps ACTUALLY complete

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 public class VariableApp {
     public static void main(String[] args) {
 
-        String favColor = "Blue";
+        String favColor = "blue";
         System.out.println(favColor);
 
         int yearUpStart = 2026;
@@ -12,7 +12,7 @@ public class VariableApp {
         String midInit = " ";
         System.out.println(midInit);
 
-        boolean hasPet = false;
+        boolean hasPet = true;
         System.out.println(hasPet);
 
         String greeting =  "Top of the morning to ya lads!";
@@ -29,6 +29,16 @@ public class VariableApp {
 
         boolean isRaining = false;
         System.out.println(isRaining);
+
+        System.out.println("My favorite color is " + favColor + ".");
+        System.out.println("I started the Year Up program in " + yearUpStart + ".");
+        System.out.println("My middle initial is: " + midInit + "because I don't have one.");
+        System.out.println("Based on the amount of cats that live in my house, do I have pets? " + hasPet + ".");
+        System.out.println("There are " + daysInTheWeek + " days in the week!");
+        System.out.println("The price of coffee is pretty decent, only $" + coffeePrice + "!");
+        System.out.println("My favorite letter is the letter " + favLetter + ".");
+        System.out.println("Is it raining today? Nah " + isRaining + " it isn't.");
+
 
     }
 }
